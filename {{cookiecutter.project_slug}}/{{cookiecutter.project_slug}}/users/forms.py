5b622247f3ba9e8075341716957fa5571f5d5e56
Samia ghezal <112901309+samiaghezal@users.noreply.github.com>
2024-11-17 15:57:56 -0500

    Update forms.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
@@ -1,5 +1,4 @@
-from allauth.account.forms import SignupForm
-from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+
 from django.contrib.auth import forms as admin_forms
 {%- if cookiecutter.username_type == "email" %}
 from django.forms import EmailField
@@ -38,17 +37,3 @@ class UserAdminCreationForm(admin_forms.UserCreationForm):
         {%- endif %}
 
 
-class UserSignupForm(SignupForm):
-    """
-    Form that will be rendered on a user sign up section/screen.
-    Default fields will be added automatically.
-    Check UserSocialSignupForm for accounts created from social.
-    """
-
-
-class UserSocialSignupForm(SocialSignupForm):
-    """
-    Renders the form when user has signed up using social accounts.
-    Default fields will be added automatically.
-    See UserSignupForm otherwise.
-    """
